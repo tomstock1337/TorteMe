ebba861a35421f6a40f92c798759ba85bec739c7
Thomas Stockwell <thomas.stockwell@gmail.com>
2024-08-25 10:37:57 -0400

    Cleaning up extra whitespace.  Replacing tabs with 2 spaces.

--- CreateSettingsWindow.lua
+++ CreateSettingsWindow.lua
@@ -1,23 +1,23 @@
 TorteMe = TorteMe or {}
 
 function TorteMe.CreateSettingsWindow()
-	local LAM = LibAddonMenu2
-	local panelName = TorteMe.name .. "SettingsPanel"
+  local LAM = LibAddonMenu2
+  local panelName = TorteMe.name .. "SettingsPanel"
 
-	local settingsWindowData = {
-		type = "panel",
-		name = TorteMe.displayName,
-		author = "|cff00ff@evainefaye|r",
-		version = TorteMe.version,
-		registerForRefresh = true,
-		registerForDefaults = true
-	}
+  local settingsWindowData = {
+    type = "panel",
+    name = TorteMe.displayName,
+    author = "|cff00ff@evainefaye|r",
+    version = TorteMe.version,
+    registerForRefresh = true,
+    registerForDefaults = true
+  }
 
-	local settingsOptionsData = {
+  local settingsOptionsData = {
     {
       type = "checkbox",
       name = "Enable HUD",
-			tooltip = "Turns on the HUD",
+      tooltip = "Turns on the HUD",
       getFunc = function()
           return TorteMe.sv.HUD.enabled
         end,
@@ -27,17 +27,17 @@ function TorteMe.CreateSettingsWindow()
             TorteMeUI:SetHidden(not value)
           end
         end,
-			disabled = function() return not TorteMe.sv.Torte.enableTortes end,
-			reference = "TORTE_ME_HUD_ENABLE"
+      disabled = function() return not TorteMe.sv.Torte.enableTortes end,
+      reference = "TORTE_ME_HUD_ENABLE"
     },
     {
-			type = "header",
-			name = "War Tortes"
-		},
+      type = "header",
+      name = "War Tortes"
+    },
     {
       type = "checkbox",
       name = "Enable Tortes",
-			tooltip = "Enable tracking and eating of tortes while in Cyrodiil",
+      tooltip = "Enable tracking and eating of tortes while in Cyrodiil",
       getFunc = function()
           return TorteMe.sv.Torte.enableTortes
         end,
@@ -45,12 +45,12 @@ function TorteMe.CreateSettingsWindow()
           TorteMe.sv.Torte.enableTortes = value
           TorteMe:TorteBuffReminderLoop()
         end,
-			reference = "TORTE_ME_ENABLE_TORTES"
+      reference = "TORTE_ME_ENABLE_TORTES"
     },
-		{
+    {
       type = "checkbox",
       name = "Auto Consume",
-			tooltip = "Automatically eat tortes while in Cyrodiil",
+      tooltip = "Automatically eat tortes while in Cyrodiil",
       getFunc = function()
           return TorteMe.sv.Torte.autoConsume
         end,
@@ -58,48 +58,48 @@ function TorteMe.CreateSettingsWindow()
           TorteMe.sv.Torte.autoConsume = value
           TorteMe:TorteBuffReminderLoop()
         end,
-			disabled = function() return not TorteMe.sv.Torte.enableTortes end,
-			reference = "TORTE_ME_TRACK_AUTO_CONSUME_TORTE"
+      disabled = function() return not TorteMe.sv.Torte.enableTortes end,
+      reference = "TORTE_ME_TRACK_AUTO_CONSUME_TORTE"
     },
-		{
-			type = "slider",
-			name = "Torte Buff Reminder Time",
-			tooltip = "How often to notify when out of tortes or needing to eat one.",
-			min = 1,
-			max = 30,
-			step = 1,
-			getFunc = function()
-				return TorteMe.sv.Torte.torteNotificationTime
-			end,
-			setFunc = function(value)
-				TorteMe.sv.Torte.torteNotificationTime = value
-			end,
-			disabled = function() return not TorteMe.sv.Torte.enableTortes end,
-			reference = "TORTE_ME_TORTE_NOTIFICATION_TIME"
-		},
-		{
-			type = "slider",
-			name = "Low Torte Count",
-			tooltip = "How many tortes you can be down to before you will be notified.",
-			min = 0,
-			max = 100,
-			getFunc = function()
-				return TorteMe.sv.Torte.torteNotifyWhenBelow
-			end,
-			setFunc = function(value)
-				TorteMe.sv.Torte.torteNotifyWhenBelow = value
-			end,
-			disabled = function() return not TorteMe.sv.Torte.enableTortes end,
-			reference = "TORTE_ME_TORTE_NOTIFY_WHEN_BELOW"
-		},
     {
-			type = "header",
-			name = "Delve Bonus"
-		},
+      type = "slider",
+      name = "Torte Buff Reminder Time",
+      tooltip = "How often to notify when out of tortes or needing to eat one.",
+      min = 1,
+      max = 30,
+      step = 1,
+      getFunc = function()
+        return TorteMe.sv.Torte.torteNotificationTime
+      end,
+      setFunc = function(value)
+        TorteMe.sv.Torte.torteNotificationTime = value
+      end,
+      disabled = function() return not TorteMe.sv.Torte.enableTortes end,
+      reference = "TORTE_ME_TORTE_NOTIFICATION_TIME"
+    },
+    {
+      type = "slider",
+      name = "Low Torte Count",
+      tooltip = "How many tortes you can be down to before you will be notified.",
+      min = 0,
+      max = 100,
+      getFunc = function()
+        return TorteMe.sv.Torte.torteNotifyWhenBelow
+      end,
+      setFunc = function(value)
+        TorteMe.sv.Torte.torteNotifyWhenBelow = value
+      end,
+      disabled = function() return not TorteMe.sv.Torte.enableTortes end,
+      reference = "TORTE_ME_TORTE_NOTIFY_WHEN_BELOW"
+    },
+    {
+      type = "header",
+      name = "Delve Bonus"
+    },
     {
       type = "checkbox",
       name = "Enable Delve Bonus Reminder",
-			tooltip = "Enable tracking Delve Bonus while in Cyrodiil",
+      tooltip = "Enable tracking Delve Bonus while in Cyrodiil",
       getFunc = function()
           return TorteMe.sv.Torte.enableDelves
         end,
@@ -107,57 +107,57 @@ function TorteMe.CreateSettingsWindow()
           TorteMe.sv.Torte.enableDelves = value
           TorteMe:DelveBonusReminderLoop()
         end,
-			reference = "TORTE_ME_ENABLE_DELVE_BONUS"
+      reference = "TORTE_ME_ENABLE_DELVE_BONUS"
     },
     {
-			type = "slider",
-			name = "Delve Bonus Reminder Time",
-			tooltip = "How often to notify when you should get a new delve bonus",
-			min = 0,
-			max = 30,
-			getFunc = function()
-				return TorteMe.sv.Torte.delveNotificationTime
-			end,
-			setFunc = function(value)
-				TorteMe.sv.Torte.delveNotificationTime = value
-			end,
-			disabled = function() return not TorteMe.sv.Torte.enableTortes end,
-			reference = "TORTE_ME_DELVE_NOTIFICATION_TIME"
+      type = "slider",
+      name = "Delve Bonus Reminder Time",
+      tooltip = "How often to notify when you should get a new delve bonus",
+      min = 0,
+      max = 30,
+      getFunc = function()
+        return TorteMe.sv.Torte.delveNotificationTime
+      end,
+      setFunc = function(value)
+        TorteMe.sv.Torte.delveNotificationTime = value
+      end,
+      disabled = function() return not TorteMe.sv.Torte.enableTortes end,
+      reference = "TORTE_ME_DELVE_NOTIFICATION_TIME"
     },
     {
       type = "header",
       name = "Debug"
-		},
+    },
     {
-			type = "checkbox",
-			name = "Enable Debug",
-			tooltip = "Show Debug Messages",
-			getFunc = function()
+      type = "checkbox",
+      name = "Enable Debug",
+      tooltip = "Show Debug Messages",
+      getFunc = function()
           return TorteMe.sv.Debug.showDebug
         end,
-			setFunc = function(value)
+      setFunc = function(value)
           TorteMe.sv.Debug.showDebug = value
         end,
-			reference = "TORTE_ME_SHOW_DEBUG",
-			disabled = false
-		},
+      reference = "TORTE_ME_SHOW_DEBUG",
+      disabled = false
+    },
     {
-			type = "slider",
-			name = "Debug Level",
-			tooltip = "Verbosity of Debug Errors",
-			min = 1,
-			max = 100,
-			getFunc = function()
+      type = "slider",
+      name = "Debug Level",
+      tooltip = "Verbosity of Debug Errors",
+      min = 1,
+      max = 100,
+      getFunc = function()
           return TorteMe.sv.Debug.showDebugLevel
         end,
-			setFunc = function(value)
+      setFunc = function(value)
           TorteMe.sv.Debug.showDebugLevel = value
         end,
-			disabled = function() return not TorteMe.sv.Debug.showDebug end,
-			reference = "TORTE_ME_DEBUG_LEVEL"
+      disabled = function() return not TorteMe.sv.Debug.showDebug end,
+      reference = "TORTE_ME_DEBUG_LEVEL"
     }
-	}
+  }
 
-	TorteMe.settingsPanel = LAM:RegisterAddonPanel("TorteMe_LAM", settingsWindowData)
-	LAM:RegisterOptionControls("TorteMe_LAM", settingsOptionsData)
+  TorteMe.settingsPanel = LAM:RegisterAddonPanel("TorteMe_LAM", settingsWindowData)
+  LAM:RegisterOptionControls("TorteMe_LAM", settingsOptionsData)
 end
--- TorteSetup.lua
+++ TorteSetup.lua
@@ -1,32 +1,32 @@
 TorteMe = TorteMe or {}
 
 --[[
-	Tun Upon Zone Into or out of Cyroddiil, when auto consume is turned on or off, or when you turn on or off tacking of tortes.
-	Does NOT run if you zone WITHIN cyrodiil.
+  Tun Upon Zone Into or out of Cyroddiil, when auto consume is turned on or off, or when you turn on or off tacking of tortes.
+  Does NOT run if you zone WITHIN cyrodiil.
 --]]
 
 function TorteMe:TorteSetup()
 
-	TorteMe:Log("Player Login, Zone, or reload detected.", true, 25)
-	local isPlayerInCyrodiil = TorteMe:IsPlayerInCyrodiil() 
-	TorteMe:Log("Is Player in Cyrodiil: " .. tostring(isPlayerInCyrodiil), true, 50)	
+  TorteMe:Log("Player Login, Zone, or reload detected.", true, 25)
+  local isPlayerInCyrodiil = TorteMe:IsPlayerInCyrodiil()
+  TorteMe:Log("Is Player in Cyrodiil: " .. tostring(isPlayerInCyrodiil), true, 50)
 
-	--Player is IN cyrodiil but torte tracking is turned off OR player is not in cyrodiil, then disable events and do nothing further
-	if (TorteMe.sv.Torte.enableTortes == false and isPlayerInCyrodiil == true) or (isPlayerInCyrodiil == false and TorteMe.torteEventsInitialized == true) then
-		TorteMe:Log("Tortes Enabled: " .. tostring(TorteMe.sv.Torte.enableTortes), true, 50)
-		TorteMeUI:SetHidden(true)
-		TorteMe:CancelCyrodiilTorteEvents()
-		return
-	end
-	if (TorteMe.sv.Torte.enableTortes == true and isPlayerInCyrodiil ) and TorteMe.torteEventsInitialized == false then
-		TorteMe:Log("Tortes Enabled: " .. tostring(TorteMe.sv.Torte.enableTortes), true, 50)
-		TorteMe:InitializeCyrodiilTorteEvents()
-	end 
-	if (isPlayerInCyrodiil) then 
-		TorteMeUI:SetHidden(not TorteMe.sv.HUD.enabled)
-	else 
-		TorteMeUI:SetHidden(true)
-	end
+  --Player is IN cyrodiil but torte tracking is turned off OR player is not in cyrodiil, then disable events and do nothing further
+  if (TorteMe.sv.Torte.enableTortes == false and isPlayerInCyrodiil == true) or (isPlayerInCyrodiil == false and TorteMe.torteEventsInitialized == true) then
+    TorteMe:Log("Tortes Enabled: " .. tostring(TorteMe.sv.Torte.enableTortes), true, 50)
+    TorteMeUI:SetHidden(true)
+    TorteMe:CancelCyrodiilTorteEvents()
+    return
+  end
+  if (TorteMe.sv.Torte.enableTortes == true and isPlayerInCyrodiil ) and TorteMe.torteEventsInitialized == false then
+    TorteMe:Log("Tortes Enabled: " .. tostring(TorteMe.sv.Torte.enableTortes), true, 50)
+    TorteMe:InitializeCyrodiilTorteEvents()
+  end
+  if (isPlayerInCyrodiil) then
+    TorteMeUI:SetHidden(not TorteMe.sv.HUD.enabled)
+  else
+    TorteMeUI:SetHidden(true)
+  end
 
   if TorteMe.sv.Torte.enableDelves == true then
     local delveBonusInfo = TorteMe:DoesPlayerHaveDelveBonus()
